5d361362c575cc7619191f8a83c1b60a2bd26cf7
juan <juanmproducoes@gmail.com>
2025-10-02 12:12:57 -0300

    ok

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,12 +2,6 @@
 import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  // trava o caminho do schema que queremos no build da Vercel
-  schema: 'prisma/schema.prisma',
-
-  // substitui o antigo "package.json#prisma.seed"
-  seed: {
-    provider: 'ts',
-    run: 'ts-node --compiler-options {"module":"commonjs"} prisma/seed.ts',
-  },
+    // trava o caminho do schema que queremos no build da Vercel
+    schema: 'prisma/schema.prisma',
 });
